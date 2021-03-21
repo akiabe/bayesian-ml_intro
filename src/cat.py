55ffd9ee9a9d18410409de58ef10def31ec9be70
Akifumi Abe <akiabe0831@gmail.com>
2021-03-22 06:54:59 +0900

    update

--- src/cat.py
+++ src/cat.py
@@ -22,10 +22,10 @@ k = np.arange(1, K+1)
 
 ### observation data ###
 N = 50
-X = np.random.multinomial(n=1, pvals=likelihood_pi, size=N)
-sum_of_X = np.sum(X, axis=0)
-print(f"X: {X[:5]}")
-print(f"sum of X: {sum_of_X}")
+s_nk = np.random.multinomial(n=1, pvals=likelihood_pi, size=N)
+sum_of_s_nk = np.sum(s_nk, axis=0)
+print(f"X: {s_nk[:5]}")
+print(f"sum of X: {sum_of_s_nk}")
 
 #plt.bar(x=k, height=sum_of_X)
 #plt.xlabel("k")
@@ -35,7 +35,7 @@ print(f"sum of X: {sum_of_X}")
 #plt.show()
 
 ### prior distribution ###
-alpha = np.array([1.0, 1.0, 1.0])
+alpha_k = np.array([1.0, 1.0, 1.0])
 
 point_vec = np.arange(0.0, 1.001, 0.02)
 x, y, z = np.meshgrid(point_vec, point_vec, point_vec)
@@ -49,29 +49,27 @@ pi /= np.sum(pi, axis=1, keepdims=True)
 pi = np.unique(pi, axis=0)
 
 prior = np.array([
-    stats.dirichlet.pdf(x=pi[i], alpha=alpha) for i in range(len(pi))
+    stats.dirichlet.pdf(x=pi[i], alpha=alpha_k) for i in range(len(pi))
 ])
 print(prior)
 
-tri_x = pi[:, 1] + pi[:, 2] / 2
-tri_y = np.sqrt(3) * pi[:, 2] / 2
-plt.scatter(tri_x, tri_y, cmap="jet")
-plt.xlabel("pi_1, pi_2")
-plt.ylabel("pi_1, pi_2")
-plt.xticks(ticks=[0.0, 1.0], labels=["(1, 0, 0)", "(0, 1, 0)"])
-plt.yticks(ticks=[0.0, 0.87], labels=["(1, 0, 0)", "(0, 0, 1)"])
-plt.title("Dirichlet Distribution")
-plt.colorbar()
+#tri_x = pi[:, 1] + pi[:, 2] / 2
+#tri_y = np.sqrt(3) * pi[:, 2] / 2
+#plt.scatter(tri_x, tri_y, cmap="jet")
+#plt.xlabel("pi_1, pi_2")
+#plt.ylabel("pi_1, pi_2")
+#plt.xticks(ticks=[0.0, 1.0], labels=["(1, 0, 0)", "(0, 1, 0)"])
+#plt.yticks(ticks=[0.0, 0.87], labels=["(1, 0, 0)", "(0, 0, 1)"])
+#plt.title("Dirichlet Distribution")
+#plt.colorbar()
 #plt.gca().set_aspect('equal')
-plt.show()
+#plt.show()
 
 ### posterior distribution ###
-#a_hat = np.sum(X) + a
-#b_hat = N - np.sum(X) + b
-#print(a_hat)
-#print(b_hat)
+alpha_hat_k = np.sum(s_nk, axis=0) + alpha_k
+print(alpha_hat_k)
 
-#posterior = stats.beta.pdf(x=prior_mu, a=a_hat, b=b_hat)
+posterior = np.array
 #print(posterior)
 
 #plt.plot(prior_mu, posterior)
