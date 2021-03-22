5807d74239e6882d6b8a0b476c0ece6751affdf0
Akifumi Abe <akiabe0831@gmail.com>
2021-03-23 07:21:04 +0900

    update

---
+++ src/gau_mean_lmd.py
@@ -0,0 +1,113 @@
+import numpy as np
+from scipy import stats
+from matplotlib import pyplot as plt
+
+### likelihood function ###
+mu_truth = 25
+lambda_truth = 0.01
+x_line = np.linspace(
+    mu_truth - 4 * np.sqrt(1 / lambda_truth),
+    mu_truth + 4 * np.sqrt(1 / lambda_truth),
+    num=1000
+)
+
+true_model = stats.norm.pdf(
+    x=x_line,
+    loc=mu_truth,
+    scale=np.sqrt(1 / lambda_truth),
+)
+print(true_model[:10])
+
+#plt.plot(x_line, true_model)
+#plt.xlabel("x")
+#plt.ylabel("density")
+#plt.title("Gaussian Distribution")
+#plt.show()
+
+### observation data ###
+N = 50
+x_n = np.random.normal(
+    loc=mu_truth,
+    scale=np.sqrt(1 / lambda_truth),
+    size=N,
+)
+print(x_n[:5])
+
+plt.hist(x=x_n, bins=50)
+plt.xlabel("x")
+plt.ylabel("count")
+plt.title("Gaussian Distribution")
+plt.show()
+
+### prior distribution ###
+m = 0
+beta = 1
+a = 1
+b = 1
+E_lambda = a / b
+
+mu_line = np.linspace(
+    mu_truth - 50,
+    mu_truth + 50,
+    num=1000,
+)
+
+prior_mu = stats.norm.pdf(
+    x=mu_line,
+    loc=m,
+    scale=np.sqrt(1 / beta / E_lambda),
+)
+print(prior[:10])
+
+#plt.plot(lambda_line, prior)
+#plt.xlabel("mu")
+#plt.ylabel("density")
+#plt.title("Gaussian Distribution")
+#plt.show()
+
+### posterior distribution ###
+#a_hat = 0.5 * N + a
+#b_hat = 0.5 * np.sum((x_n - mu)**2) + b
+#print(a_hat)
+#print(b_hat)
+
+#posterior = stats.gamma.pdf(
+#    x=lambda_line,
+#    a=a_hat,
+#    scale=1 / b_hat,
+#)
+
+#print(posterior[:10])
+
+#plt.plot(lambda_line, posterior)
+#plt.vlines(
+#    x=lambda_truth,
+#    ymin=0,
+#    ymax=max(posterior),
+#    color='red',
+#    linestyle='--',
+#)
+#plt.xlabel("lambda")
+#plt.ylabel("density")
+#plt.title("Gaussian Distribution")
+#plt.show()
+
+### predict distribution ###
+#mu_s_hat = mu
+#lambda_s_hat = a_hat / b_hat
+#nu_s_hat = 2 * a_hat
+
+#predict = stats.t.pdf(
+#    x=x_line,
+#    df=nu_s_hat,
+#    loc=mu_s_hat,
+#    scale=np.sqrt(1 / lambda_s_hat),
+#)
+#print(predict[:10])
+
+#plt.plot(x_line, predict, label='predict')
+#plt.plot(x_line, true_model, label='true', linestyle='--')
+#plt.xlabel("x")
+#plt.ylabel("density")
+#plt.title("Gaussian Distribution")
+#plt.show()
